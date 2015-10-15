364edb13679baf445c15b1bbf14e89cf4bfda313
hamapon <hamapon.k.h@gmail.com>
2015-10-16 02:01:50 +0900

    test

--- app/src/main/java/com/example/smartglass/MainActivity.java
+++ app/src/main/java/com/example/smartglass/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Mat u;
+        Mat o;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
