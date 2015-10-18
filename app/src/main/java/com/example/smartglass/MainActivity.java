81aaa74b1361e1ca19650fa79546f02b582fdaaa
hamapon <hamapon.k.h@gmail.com>
2015-10-18 20:06:35 +0900

    opencvなしのカメラプレビュー

--- app/src/main/java/com/example/smartglass/MainActivity.java
+++ app/src/main/java/com/example/smartglass/MainActivity.java
@@ -1,53 +1,74 @@
 package com.example.smartglass;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import org.opencv.core.Mat;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    //カメラの宣言
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.main);
+        //フルスクリーンの指定
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //カメラプレビュー画面の設定
+        SurfaceView cameraPreview= (SurfaceView)findViewById(R.id.preview);
+        //サーフェイスホルダー生成
+        cameraPreview.getHolder().addCallback(previewCallback);
+        //サーフェイスホルダーのタイプを設定(外部バッファの使用)
+        cameraPreview.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    //サーフェイスホルダーの「Callback()」メソッドを実装
+    private SurfaceHolder.Callback previewCallback = new SurfaceHolder.Callback() {
+        private Camera camera;
+        // サーフェイス生成処理
+        public void surfaceCreated(SurfaceHolder holder) {
+            // カメラ初期化
+                    try {
+                        // カメラのオープン
+                        camera = Camera.open();
+                        // プレビューディスプレイのセット
+                        camera.setPreviewDisplay(holder);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+        // サーフェイス変更処理
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            // プレビューを停止
+            camera.stopPreview();
+            // カメラのパラメータを取得
+            Camera.Parameters params = camera.getParameters();
+            // パラメータにプレビュー表示のサイズを設定
+            params.setPreviewSize(params.getPreviewSize().width, params.getPreviewSize().height);
+            // パラメータのセット
+            camera.setParameters(params);
+            // カメラプレビュー開始
+            camera.startPreview();
+        }
+        // サーフェイス開放処理
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // カメラプレビュー停止
+            camera.stopPreview();
+            // カメラリリース
+            camera.release();
+        }
+    };
 }
