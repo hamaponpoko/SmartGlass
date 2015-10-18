d1f5ba1b67993d25a49f6bbc0a3ae9c67953a702
hamapon <hamapon.k.h@gmail.com>
2015-10-19 03:12:46 +0900

    フレーム間差分

--- app/src/main/java/com/example/smartglass/MainActivity.java
+++ app/src/main/java/com/example/smartglass/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.smartglass;
 
 import android.app.Activity;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
@@ -9,11 +12,15 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String  TAG              = "OCVSample::Activity";
     private CameraBridgeViewBase mCameraView;
     private Mat mOutputFrame;
+    private Mat Frame0,Frame;
+    private Mat dif,difb0,difb;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -65,21 +72,36 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         // Mat(int rows, int cols, int type)
         // rows(行): height, cols(列): width
         mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
+        Frame = new Mat(height, width, CvType.CV_8UC1);
+        Frame0 = new Mat(height, width, CvType.CV_8UC1);
+        difb0 = new Mat(height, width, CvType.CV_8UC1);
+        difb = new Mat(height, width, CvType.CV_8UC1);
+        dif = new Mat(height, width, CvType.CV_8UC1);
     }
 
     @Override
     public void onCameraViewStopped() {
         // カメラプレビュー終了時に呼ばれる
         mOutputFrame.release();
+        Frame0.release();
+        Frame.release();
+        dif.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         // フレームをキャプチャする毎(30fpsなら毎秒30回)に呼ばれる
-        // Cannyフィルタをかける
-        Imgproc.Canny(inputFrame.gray(), mOutputFrame, 80, 100);
-        // ビット反転
-        Core.bitwise_not(mOutputFrame, mOutputFrame);
+        Frame=inputFrame.gray();
+        // 差分計算
+        Core.absdiff(Frame0, Frame, dif);
+        //差分画像の二値化
+        Imgproc.threshold(dif, difb, 30, 255, Imgproc.THRESH_BINARY);
+        //二値化された差分画像の共通部分を取得
+        Core.bitwise_and(difb0, difb, mOutputFrame);
+        //更新
+        Frame0=Frame;
+        difb0=difb;
+
         return mOutputFrame;
     }
 }
